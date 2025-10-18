54c8cf9a64bc053f0d1cca53933e868aaf6e6128
Masato Nitta <newmtube07@gmail.com>
2025-10-19 00:22:19 +0900

    update

--- server/prisma/migrations/20250504143130_init/migration.sql
+++ server/prisma/migrations/20250504143130_init/migration.sql
@@ -10,12 +10,12 @@ CREATE TABLE "note" (
 );
 
 -- CreateTable
-CREATE TABLE "User" (
+CREATE TABLE "user" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "age" INTEGER NOT NULL DEFAULT 0,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
 );
